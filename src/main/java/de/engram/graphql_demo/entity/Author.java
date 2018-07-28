f3ce371b8a69082eab544aab9e2553ffa0ad2bde
Erik Legenhausen <erik.legenhausen@engram.de>
2018-07-28 10:23:30 +0200

    graphql libs upgarded to 5.x, refactorings

--- src/main/java/de/engram/graphql_demo/entity/Author.java
+++ src/main/java/de/engram/graphql_demo/entity/Author.java
@@ -16,10 +16,6 @@ public class Author {
 	private String firstName;
 	private String lastName;
 
-	public Author(Long id) {
-		this.id = id;
-	}
-
 	public Author(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
